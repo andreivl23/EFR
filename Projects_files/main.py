44eef6c03124d9c5f3a1dd858eebdf71be05072f
Andrei <anvlsnko@gmail.com>
2023-09-22 23:46:08 +0300

    Whole lot of changes, added moveto function and while loop

--- Projects_files/main.py
+++ Projects_files/main.py
@@ -10,62 +10,73 @@ connection = mysql.connector.connect(
          autocommit=True
          )
 
-def getcurrentstationname(Location):
-    sql = "SELECT StationName FROM Stations, Game WHERE StationID = Location AND Game.location ='" + Location + "'"
+def getcurrentstationname(GameID):
+    sql = f"SELECT StationName FROM Stations, Game WHERE StationID = Location AND GameID ='{GameID}'"
     cursor = connection.cursor()
     cursor.execute(sql)
     result = cursor.fetchall()
     return result
 
 
-def getstationname(i):
-    sql = f"SELECT StationName from Stations WHERE StationID = '{i}'"
+def getstationid(StationName):
+    sql = f"SELECT StationID from Stations WHERE StationName = '{StationName}'"
     cursor = connection.cursor()
     cursor.execute(sql)
     neighbor = cursor.fetchall()
     return neighbor
 
-
-def start(ScreenName, Balance):
+def start(GameID, ScreenName, Balance):
     Location = str(random.randint(1,61))
     #print(ScreenName,Location,Balance)
-    sql = "INSERT INTO Game (ScreenName, Location, Balance) VALUES ("
-    sql += f"'{ScreenName}'," + f"{Location}," + f"'{Balance}')"
+    sql = "INSERT INTO Game (GameID, ScreenName, Location, Balance) VALUES ("
+    sql += f"{GameID},'{ScreenName}'," + f"{Location}," + f"'{Balance}')"
     cursor = connection.cursor()
     cursor.execute(sql)
-
     return Location
 
 
 def getneighbors(Location):
-    sql = f"SELECT StationName from Stations, Connections WHERE StationID2 = StationID AND StationID1 = '{Location}'"
+    sql = f"SELECT StationName, StationID from Stations, Connections WHERE StationID2 = StationID AND StationID1 = '{Location}'"
     cursor = connection.cursor()
     cursor.execute(sql)
     neighborsname = cursor.fetchall()
     return neighborsname
-
-def getcoordinates(station):
-    sql = "SELECT latitude, longitude FROM Stations"
-    sql += " WHERE StationID='" + station + "'" +" OR StationName ='" + station + "'"
-    cursor = connection.cursor()
-    cursor.execute(sql)
-    result = cursor.fetchall()
-    return result
 
 def cleartable():
     sql = "DELETE FROM Game"
     cursor = connection.cursor()
     cursor.execute(sql)
     return
-def main():
-    ScreenName = input("Choose your name: ")
-    Balance = str(random.randint(20,100))
-    Location = start(ScreenName, Balance)
-    StationName = getcurrentstationname(Location)
-    neighbors = getneighbors(Location)
-    print(f"{ScreenName}, you are at station {StationName[0][0]}\nYour balance is {Balance} rubles")
-    for station in neighbors:
-        print(f"You can travel to {station[0]}")
 
+def moveto(station):
+    sql = f"UPDATE Game SET Location = '{station}' "
+    cursor = connection.cursor()
+    cursor.execute(sql)
+    return
+
+def main():
+    GameID = random.randint(1, 999999)
+    ScreenName = input("Choose your name: ")
+    Balance = str(random.randint(20, 100))
+    Location = start(GameID, ScreenName, Balance)
+    chosed = 0
+
+    while chosed != "stop":
+        StationName = getcurrentstationname(GameID)
+        StationID = getstationid(StationName[0][0])
+        neighbors = getneighbors(StationID[0][0])
+        print(f"...\n{ScreenName}, you are at station {StationName[0][0]}\nYour balance is {Balance} rubles")
+        print("Connected stations: ")
+        for station in neighbors:
+            print(f"{station[0]} (ID: {station[1]})")
+
+            #StationID = station[1]
+            #options = []
+            #options.append(StationID)
+
+        chosed = input("Where to?: ")
+        moveto(chosed)
+        #StationName = getcurrentstationname(GameID)
+        #print(StationName[0][0])
 main()
 cleartable()
