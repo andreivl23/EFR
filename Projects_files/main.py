d3bea0d7b980467e4dfd06c7bea1fd0fcdc00584
Pavel Degterev <pdegeterev@gmail.com>
2023-09-26 14:15:36 +0300

    test commit

--- Projects_files/main.py
+++ Projects_files/main.py
@@ -22,6 +22,7 @@ def get_stations():
     return result
 
 
+
 def getcurrentstationname(gameid):
     sql = f"SELECT StationName FROM Stations WHERE StationID = {gameid}"
     cursor = connection.cursor()
