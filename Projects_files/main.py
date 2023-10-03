69fca81e5a8540ffefa7b84072188751ff367ff5
Andrei <anvlsnko@gmail.com>
2023-10-03 15:18:48 +0300

    changed logic of start(), added airplane_dictionary

--- Projects_files/main.py
+++ Projects_files/main.py
@@ -112,7 +112,7 @@ def get_stations():
 
 
 def get_events():
-    sql = "SELECT * FROM events;"
+    sql = "SELECT id, name, balance, probability FROM events;"
     cursor = connection.cursor(dictionary=True)
     cursor.execute(sql)
     result = cursor.fetchall()
@@ -127,11 +127,14 @@ def start(resource, current_station, player, stations):
 
     events = get_events()
     events_list = []
+    airplane_dictionary = events[0]
     for event in events:
         for i in range(0, event['probability'], 1):
             events_list.append(event['id'])
 
     t_stations = stations[1:].copy()
+
+    events_list = list(filter((1).__ne__, events_list))
     random.shuffle(t_stations)
 
     for i, event_id in enumerate(events_list):
@@ -140,7 +143,7 @@ def start(resource, current_station, player, stations):
         cursor = connection.cursor(dictionary=True)
         cursor.execute(sql)
 
-    return g_id
+    return g_id, airplane_dictionary
 
 
 def create_game():
@@ -152,9 +155,9 @@ def create_game():
 
     all_stations = get_stations()
     current_station = all_stations[0]['StationID']
-    game_id = start(vodka_balance, current_station, screen_name, all_stations)
+    game_id, airplane_dictionary = start(vodka_balance, current_station, screen_name, all_stations)
 
-    return current_station, game_id
+    return current_station, game_id, airplane_dictionary
 
 
 def moveto(station):
@@ -218,8 +221,6 @@ def update_balance(amount, game_id):
 
 
 
-
-
 def main():
     menu()
     while True:
@@ -227,7 +228,7 @@ def main():
 
         ##################### Start #########################
 
-        current_station, game_id = create_game()
+        current_station, game_id, airplane_dictionary = create_game()
 
         while True:
             screen_refresh()
