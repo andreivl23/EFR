880a9893b07f51df4b81576b712b5547ec82a66f
andreivl23 <127105524+andreivl23@users.noreply.github.com>
2023-10-10 22:27:42 +0300

    Update connection main.py

--- Projects_files/main.py
+++ Projects_files/main.py
@@ -13,8 +13,8 @@ connection = mysql.connector.connect(
          host='172.232.129.9',
          port=3306,
          database='efr_mini',
-         user='root',
-         password='password',
+         user='American',
+         password='123321',
          autocommit=True
          )
 
